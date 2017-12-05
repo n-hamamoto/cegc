57c6e2421473703eb6c2004708e1843c09a77271
root <root@srp.media.gunma-u.ac.jp>
2017-12-05 18:55:32 +0900

    1st commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<?php include("conf/config.php") ?>
+<?php include("lib/header.php") ?>
+</head>
+<body>
+<div id="login">
+<h1>情報倫理eラーニング成績確認システム</h1>
+<br>
+<br>
+<a href="secure">ログイン</a>
+</div>
+</body>
+</html>
