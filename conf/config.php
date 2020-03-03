43f713420b0ee7557f2473398d394d1b30550fea
n-hamamoto <n.hamamoto@gunma-u.ac.jp>
2020-03-03 14:53:48 +0900

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -18,7 +18,7 @@ $idpdb = ''; 		#データベース名
 //useridを小文字に統一する
 $lowercaseId = 1;
 
-#$documentRoot = $_SERVER['SERVER_NAME']."/GU/cyberethics-log/";
-$documentRoot = ""."/"; #本システムのURL(例: example.ac.jp/)を指定してください
+#$documentRoot = $_SERVER['SERVER_NAME'].'/GU/cyberethics-log/';
+$documentRoot = ''.'/'; #本システムのURL(例: example.ac.jp/)を指定してください
 
 ?>
