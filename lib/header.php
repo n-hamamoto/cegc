0522fbc1428baa2aff9bcbf397099d58954dd62c
n-hamamoto <n.hamamoto@gunma-u.ac.jp>
2023-06-08 14:31:09 +0900

    jquery directry downloaded from CDN

--- lib/header.php
+++ lib/header.php
@@ -1,7 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <base href="https://<?php echo $documentRoot ?>">
 
-<script type="text/javascript" src="https://<?php echo $documentRoot ?>/js/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://<?php echo $documentRoot ?>/js/common.js"></script>
 
 <link rel="stylesheet" href="https://<?php echo $documentRoot ?>/css/common.css">
